4d36d66f57989f3123067b4c5d282ae6c6660220
Alexander Schulze <aschulze@innotrade.com>
2020-01-29 15:50:24 +0100

    Cleaned up index.js from old obsolete stuff.

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-// Innotrade Enapso SPARQL Toolbox
+// Innotrade Enapso - SPARQL Toolbox
 // (C) Copyright 2019-2020 Innotrade GmbH, Herzogenrath, NRW, Germany
 // Authors: Alexander Schulze
-
-/*
-const getClassSchemaMeta = require('./lib/schema/getClassSchemaMeta')
-const getIndividualsMeta = require('./lib/instances/getIndividualsMeta')
-const buildSelectQuery = require('./lib/query/buildSelectQuery')
-*/
 
 const ClassCache = require('./lib/classCache');
 const Class = require('./lib/classes');
@@ -18,10 +12,4 @@ module.exports = {
     Class,
     Generator,
     Prefix, PrefixManager
-
-    /*
-    getClassSchemaMeta: getClassSchemaMeta,
-	getIndividualsMeta: getIndividualsMeta,
-    buildSelectQuery: buildSelectQuery
-    */
 }
