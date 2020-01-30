b64b1b4f1aef5724ca1486286068fc6fe6aed034
Alexander Schulze <aschulze@innotrade.com>
2020-01-30 10:23:13 +0100

    Fixed issue with references in index.js. Updated to 0.0.5.

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 // (C) Copyright 2019-2020 Innotrade GmbH, Herzogenrath, NRW, Germany
 // Authors: Alexander Schulze
 
-const ClassCache = require('./lib/classCache');
-const Class = require('./lib/classes');
-const Generator = require('./lib/generator');
+const { ClassCache } = require('./lib/classCache');
+const { Class } = require('./lib/classes');
+const { Generator } = require('./lib/generator');
+const { Property } = require('./lib/properties');
 const { Prefix, PrefixManager } = require('./lib/prefixManager');
 
 module.exports = {
-    ClassCache,
-    Class,
-    Generator,
+    ClassCache, Class,
+    Generator, Property,
     Prefix, PrefixManager
 }
