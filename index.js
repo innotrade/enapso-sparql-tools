5b4fd8a07946c4328ff2908e3244a734da31498c
Alexander Schulze <a.schulze@innotrade.de>
2020-05-31 13:42:45 +0200

    Merging various branches (35/40/41), to be code-reviewed!

--- index.js
+++ index.js
@@ -2,26 +2,30 @@
 // (C) Copyright 2019-2020 Innotrade GmbH, Herzogenrath, NRW, Germany
 // Authors: Alexander Schulze
 
-const
-	{ ClassCache } = require('./lib/classCache'),
-	{ Entity } = require('./lib/entity'),
-	{ Class } = require('./lib/classes'),
-	{ Generator } = require('./lib/generator'),
-	{ Property } = require('./lib/properties'),
-	{ Prefix, PrefixManager } = require('./lib/prefixManager')
-	;
+const { ClassCache } = require('./lib/classCache'),
+  { Entity } = require('./lib/entity'),
+  { Class } = require('./lib/classes'),
+  { Generator } = require('./lib/generator'),
+  { Property } = require('./lib/properties'),
+  { Prefix, PrefixManager } = require('./lib/prefixManager');
 
 const ensptools = {
-	ClassCache, 
-	Entity, Class,
-	Generator, Property,
-	Prefix, PrefixManager
-}
+  ClassCache,
+  Entity,
+  Class,
+  Generator,
+  Property,
+  Prefix,
+  PrefixManager
+};
 
 module.exports = {
-	ensptools,
-	ClassCache, 
-	Entity, Class,
-	Generator, Property,
-	Prefix, PrefixManager
-}
+  ensptools,
+  ClassCache,
+  Entity,
+  Class,
+  Generator,
+  Property,
+  Prefix,
+  PrefixManager
+};
